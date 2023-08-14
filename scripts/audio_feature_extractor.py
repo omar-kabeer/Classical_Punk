53ead31d8a60b23a57b69ca44a71760ebd843384
Umar Kabir <61500640+omar-kabeer@users.noreply.github.com>
2023-08-14 14:18:28 +0100

    adding notebooks & feature extractor

--- scripts/audio_feature_extractor.py
+++ scripts/audio_feature_extractor.py
@@ -1,92 +1,46 @@
 import os
 import librosa
 import numpy as np
-
-# Function to extract features from an audio file
-def extract_features(audio_file_path):
-    features = []
-
-    # Load audio
-    audio, sr = librosa.load(audio_file_path, sr=None)
-
-    # Extract features
-    mfccs = librosa.feature.mfcc(audio, sr=sr)
-    spectral_centroid = librosa.feature.spectral_centroid(audio, sr=sr)
-    spectral_bandwidth = librosa.feature.spectral_bandwidth(audio, sr=sr)
-    spectral_rolloff = librosa.feature.spectral_rolloff(audio, sr=sr)
-    spectral_contrast = librosa.feature.spectral_contrast(audio, sr=sr)
-    spectral_flatness = librosa.feature.spectral_flatness(audio)
-    harmonicity = librosa.effects.harmonic(audio)
-    percussiveness = librosa.effects.percussive(audio)
-    spectral_flux = librosa.onset.onset_strength(audio, sr=sr)
-    spectral_slope = librosa.feature.spectral_slope(audio, sr=sr)
-    zcr = librosa.feature.zero_crossing_rate(audio)
-    energy = np.sum(audio ** 2)
-    tempo, _ = librosa.beat.beat_track(audio, sr=sr)
-    pitch, _ = librosa.piptrack(audio, sr=sr)
-    loudness = librosa.amplitude_to_db(librosa.feature.rms(audio))
-    duration = librosa.get_duration(y=audio, sr=sr)
-    complexity = np.mean(np.diff(audio))
-    timbre = np.mean(spectral_contrast)
-    spectral_energy = np.sum(np.abs(np.fft.fft(audio)) ** 2)
-    temporal_energy = np.sum(audio ** 2)
-    energy_flux = np.diff(temporal_energy)
-    energy_distribution = temporal_energy / np.sum(temporal_energy)
-    energy_entropy = -np.sum(energy_distribution * np.log2(energy_distribution + 1e-12))
-
-    # Append features to the list
-    features.extend([
-        mfccs,
-        spectral_centroid,
-        spectral_bandwidth,
-        spectral_rolloff,
-        spectral_contrast,
-        spectral_flatness,
-        harmonicity,
-        percussiveness,
-        spectral_flux,
-        spectral_slope,
-        zcr,
-        energy,
-        tempo,
-        pitch,
-        loudness,
-        duration,
-        complexity,
-        timbre,
-        spectral_energy,
-        temporal_energy,
-        energy_flux,
-        energy_distribution,
-        energy_entropy,
-    ])
-
-    return features
-
-
-# List to store extracted features
-all_features = []
-
-# Loop through audio files and extract features
+import pandas as pd
+genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+data = []
 for genre in genres:
     genre_dir = f'../data/genres/{genre}'
     for filename in os.listdir(genre_dir):
-        if filename.endswith(".wav"):
+        if filename.endswith('.wav'):
             audio_path = os.path.join(genre_dir, filename)
-            features = extract_features(audio_path)
-            all_features.append(features)
+            y, sr = librosa.load(audio_path)
 
-# Convert the list of feature sets into a DataFrame
-feature_columns = [
-    "mfccs", "spectral_centroid", "spectral_bandwidth", "spectral_rolloff", 
-    "spectral_contrast", "spectral_flatness", "harmonicity", "percussiveness", 
-    "spectral_flux", "spectral_slope", "zcr", "energy", "tempo", "pitch", 
-    "loudness", "duration", "complexity", "timbre", "spectral_energy", 
-    "temporal_energy", "energy_flux", "energy_distribution", "energy_entropy"
-]
+            # Extract features
+            central_moments = np.asarray([np.mean((y - np.mean(y)) ** i) for i in range(1, 5)])
+            zero_crossing_rate = librosa.feature.zero_crossing_rate(y)
+            rmse = librosa.feature.rms(y=y)
+            tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+            spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr)
+            spectral_rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+            mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+            chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+            spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
+            spectral_bandwidth = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+            spectral_flatness = librosa.feature.spectral_flatness(y=y)
+            loudness = librosa.amplitude_to_db(librosa.feature.rms(y=y))
+            complexity = np.mean(np.diff(y))
+            timbre = np.mean(spectral_contrast)
+            spectral_energy = np.sum(np.abs(np.fft.fft(y)) ** 2)
+            temporal_energy = np.sum(y ** 2)
+            harmonicity = librosa.effects.harmonic(y=y)
+            percussiveness = librosa.effects.percussive(y=y)
 
-feature_df = pd.DataFrame(all_features, columns=feature_columns)
+            # Add features to dataframe
+            features = [genre, filename, beats, sr, central_moments, zero_crossing_rate[0], rmse[0],
+                        tempo, spectral_contrast, spectral_rolloff, mfccs, chroma, spectral_centroid, spectral_bandwidth,
+                        spectral_flatness, loudness, complexity, timbre, spectral_energy, temporal_energy, harmonicity, percussiveness]
+            data.append(features)
+
+df = pd.DataFrame(data, columns=['Genre', 'Filename', 'Beats', 'SR', 'Central Moments', 'Zero Crossing Rate', 'RMSE', 'Tempo', 'Spectral Contrast',
+                                'Spectral Roll-off', 'MFCC', 'Chroma', 'Spectral Centroid', 'Spectral Bandwidth',
+                                'Spectral Flatness', 'Loudness', 'Complexity', 'Timbre', 'Spectral Energy', 'Temporal Energy', 'Harmonicity', 'Percussiveness'])
 
 # Save the DataFrame as a CSV file
-csv_filename = "audio_features.csv"
-feature_df.to_csv(csv_filename, index=False)
+csv_filename = "../data/data.csv"
+df.to_csv(csv_filename, index=False)
